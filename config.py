4960afc3714762478772cb704b0c196e17ada74b
Saajan Saini <sainisaaji@gmail.com>
2026-02-19 23:55:50 +0100

    l

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-prod'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///tper.db'
+    db_url = os.environ.get('DATABASE_URL') or 'sqlite:///tper.db'
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'admin'
